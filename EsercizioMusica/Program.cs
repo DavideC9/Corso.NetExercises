c9a9d41cda1e0f7bbe2962e518fb7027b9d7ed31
DavideC9 <112826328+DavideC9@users.noreply.github.com>
2022-11-07 09:28:19 +0100

    Update Program.cs

--- EsercizioMusica/Program.cs
+++ EsercizioMusica/Program.cs
@@ -167,19 +167,7 @@ namespace EsercizioMusica
 
 
 
-        interface IPlayer
-        {
-            public void Play(int SongId);
-            public void ShowPlaying();
-            public void StartPlaying();
-            public void Stop();
-            public void Pause();
-            public void Rate();
-            public void Forward();
-            public void Backward();
-            public void ListSongs();
-
-        }
+        
         public interface Iplayer
         {
             void play();
